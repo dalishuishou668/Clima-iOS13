069477bc4be6fdededd8af1a795e124af97f789f
dalishuishou668 <liuqiaoyi1990@163.com>
2023-07-27 17:05:22 -0700

    add ui textfield

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -2,15 +2,51 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // dismiss keyboard
+        print(searchTextField.text!)
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // text field should report back to view controller, self refers the current view controller
+        searchTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        // when return button pressed on the keyboard
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }else{
+            textField.placeholder = "Type somthing"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
     }
 
 
