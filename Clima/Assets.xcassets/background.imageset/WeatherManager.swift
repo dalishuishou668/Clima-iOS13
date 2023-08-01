5c8dc7c03446fa6ddb443e98708faed592fa67ae
dalishuishou668 <liuqiaoyi1990@163.com>
2023-08-01 09:53:33 -0700

    perform url request

--- Clima/Assets.xcassets/background.imageset/WeatherManager.swift
+++ Clima/Assets.xcassets/background.imageset/WeatherManager.swift
@@ -15,6 +15,37 @@ struct WeatherManager{
     func fetchWeather(cityName:String){
         let urlString = "\(weatherURL)&q=\(cityName)"
         //print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        // create url
+        if let url = URL(string: urlString){
+            
+            // create url session object that perform networking
+            let session = URLSession(configuration: .default)
+            
+            // give task(completed task will trigger completionhandler)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            //start task
+            task.resume()
+            
+            
+        }
+    }
+    
+    func handle(data:Data?, response:URLResponse?, error:Error?){
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            //print(dataString)
+        }
+        
     }
     
 }
