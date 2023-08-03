411292a15c5893deac2d1e5bff2b86d7f10e09b8
dalishuishou668 <liuqiaoyi1990@163.com>
2023-08-03 12:02:30 -0700

    updates location function working

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -26,6 +26,14 @@ class WeatherViewController: UIViewController{
         // text field should report back to view controller, self refers the current view controller
         searchTextField.delegate = self
     }
+    
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
+    
+    
 }
 
 // MARK: - UITextFieldDelegate
@@ -88,8 +96,12 @@ extension WeatherViewController: WeatherManagerDelegate{
 // MARK: - CLLocationManagerDelegate
 
 extension WeatherViewController: CLLocationManagerDelegate{
+    
+    
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last{
+            locationManager.stopUpdatingLocation()
             let lat = location.coordinate.latitude
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(latitude: lat, longitute:lon)
